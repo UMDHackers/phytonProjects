850a01927d8ee5644c8a5b0fdba9ea80bc86d81a
Parth Desai <parthdesai95@gmail.com>
2016-01-02 14:46:28 -0500

    changed

--- alaram.py
+++ alaram.py
@@ -2,29 +2,30 @@ from time import gmtime, strftime
 import time
 import winsound
 from msvcrt import getch
-pick = False
+import threading
+
+active_alarm = dict()
 Freq = 2500 # Set Frequency To 2500 Hertz
 Dur = 500 # Set Duration To 1000 ms == 1 second
 alarm = "0"
 key = 0
+def check_time():
+	#print(strftime("%I:%M:%S %p", time.localtime()))
+	current = strftime("%I:%M:%S %p", time.localtime())
+	for key in active_alarm:
+		if current == active_alarm[key]:
+			for x in range(0, 3):
+				print "WAKE UP"
+				print "GET UP"
+				winsound.Beep(Freq,Dur)
+				del active_alarm[key]
 
-while True:
-    #print(strftime("%I:%M:%S %p", time.localtime()))
-    current = strftime("%I:%M:%S %p", time.localtime())
-    if current == alarm :
-        pick = False
-        alarm = "0"
-        i = 0
-        while i != 6:
-            print "WAKE UP"
-            print "GET UP"
-            winsound.Beep(Freq,Dur)
-            i += 1
+def pick_time():
+	alarm = raw_input("Enter time you would like to wake up please! HH:MM:SS AM/PM ")
+	
+try: 
+	threading.Thread(target=check_time, args=())
+except:
+	print "fail"
 
-
-    if pick != True :
-        print("Enter time you would like to wake up please! HH:MM:SS AM/PM")
-        alarm = raw_input()
-        pick = True
-
-    #time.sleep(1)
+pick_time()
